b66dfc1bb8c8164bc607acca36372a3e89d7502b
amanmandalpy <amanmandal1@gmail.com>
2026-07-15 19:56:20 +0530

    Initial Django project setup with modular app structure

--- apps/common/apps.py
+++ apps/common/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
 
-class CommonConfig(AppConfig):
-    name = 'common'
+class EmployeesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.common"
--- apps/leaves/apps.py
+++ apps/leaves/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
 
-class LeavesConfig(AppConfig):
-    name = 'leaves'
+class EmployeesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.leaves"
