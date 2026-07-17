6a69f61aaf87bc2cb2e613e24a0fbc5c7670f2cc
amanmandalpy <amanmandal1@gmail.com>
2026-07-17 11:10:11 +0530

    Configure Django admin for custom user model

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "email",
+        "role",
+        "is_staff",
+        "is_active",
+    )
+
+    ordering = ("email",)
+
+    search_fields = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Roles", {"fields": ("role",)}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "role",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
